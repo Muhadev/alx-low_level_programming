3214c0be4abec4d30fdba159be6f5ac1d7b8468a
Muhadev <muhammedfayemi@gmail.com>
2023-07-13 13:44:28 -0700

    6-size.c is added

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("The size of the int is: %lu.\n", (unsigned long)sizeof(i));
+printf("The size of the double is: %lu.\n", (unsigned long)sizeof(d));
+printf("The size of the float is: %lu.\n", (unsigned long)sizeof(f));
+printf("The size of the char is: %lu.\n", (unsigned long)sizeof(c));
+return (0);
+}
