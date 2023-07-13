96d31faf79f4fb8d3d752838e1b64b0fe87e68e1
Muhadev <muhammedfayemi@gmail.com>
2023-07-13 13:54:10 -0700

    6-size.c is added

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,7 @@ printf("The size of the int is: %zu.\n", sizeof(int));
 printf("The size of the double is: %zu.\n", sizeof(double));
 printf("The size of the float is: %zu.\n", sizeof(float));
 printf("The size of the char is: %zu.\n", sizeof(char));
+printf("The size of the long int is: %zu.\n", sizeof(long int));
+printf("The size of the long long int is: %zu.\n", sizeof(long long int));
 return (0);
 }
