50ae55ed2089ac205015d612d10f267b9dd631f3
Muhadev <muhammedfayemi@gmail.com>
2023-07-14 12:16:38 -0700

    5-print_numbers.c is added

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,11 +7,10 @@
 int main(void)
 {
 int num;
-for (num = 0; num <=10; num++)
+for (num = 0; num < 10; num++)
 {	
-	putchar('0' + num);
-	putchar(' ');
+	printf("%d", num);
 }
-putchar('\n');
+printf("\n");
 return (0);
 }
