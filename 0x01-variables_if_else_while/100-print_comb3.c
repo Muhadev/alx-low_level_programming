e01456e116190c225acecf3dcdb3c85504dc6a08
Muhadev <muhammedfayemi@gmail.com>
2023-07-15 07:39:32 -0700

    100-print_comb3.c  is added

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,12 +11,12 @@ for (digit1 = 0; digit1 < 10; digit1++)
 {
 for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
-	putchar('0' + digit1);
-	putchar('0' + digit2);
-if (digit1 != 8 || digit2 != 9)
+	putchar(digit1 + '0');
+	putchar(digit2 + '0');
+if (digit1 < 8 || digit2 < 9)
 {
-	putchar(' ');
 	putchar(',');
+	putchar(' ');
 }
 }
 putchar('\n');
