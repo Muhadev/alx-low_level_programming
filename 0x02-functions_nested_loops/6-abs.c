c3d3cb006bbc0cc21349a667e08f4291ba10161f
Muhadev <muhammedfayemi@gmail.com>
2023-07-19 13:33:19 -0700

    6-abs.c is added

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,14 +6,10 @@
  *
  * Return: The value of the last digit
  */
-int _abs(int num)
+int _abs(int n)
 {
-int last_digit;
-	last_digit = num % 10;
-if (last_digit < 0)
-{
-        last_digit = -last_digit;
-}
-	_putchar(last_digit + '0');
-return last_digit;
+if (n >= 0)
+return (n);
+else
+return (-n);
 }
