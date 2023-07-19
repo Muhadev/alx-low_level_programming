f87f4544d381a59038e601307e5919abecee4466
Muhadev <muhammedfayemi@gmail.com>
2023-07-19 12:56:33 -0700

    5-sign.c is added

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,9 +10,10 @@
  */
 int print_sign(int n)
 {
-if (n > 0) {
+if (n > 0)
+{
 	_putchar('+');
-return 1;
+return (1);
 }
 else if (n == 0)
 {
@@ -20,7 +21,7 @@ else if (n == 0)
 return (0);
 }
 else
-{ 
+{
 	_putchar('-');
 return (-1);
 }
