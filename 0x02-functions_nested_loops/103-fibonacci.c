3bfe157ce9b645e988753e933859ebb2400050e8
Muhadev <muhammedfayemi@gmail.com>
2023-07-19 17:21:03 -0700

    104-fibonacci.c is added

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,10 +11,12 @@ int main() {
 int fib[50];
 	fib[0] = 1;
 	fib[1] = 2;
-for (int i = 2; i < 50; i++) {
+for (int i = 2; i < 50; i++)
+{
 	fib[i] = fib[i - 1] + fib[i - 2];
 }
-for (int i = 0; i < 50; i++) {
+for (int i = 0; i < 50; i++)
+{
 	printf("%d", fib[i]);
 if (i < 49) {
 	printf(", ");
