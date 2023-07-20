ab5007e740fd65a7f1fc45dc6ad8f8f870c0881c
Muhadev <muhammedfayemi@gmail.com>
2023-07-20 09:38:37 -0700

    0-isupper.c is added

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,14 @@
 
 /**
  * _islower - Checks if a character is lowercase
- *
  * @c: The character to check
- *
- * Return:
- * 1 if c is lowercase, 0 otherwise
+ * Return: 1 if c is upper letter, 0 otherwise
  */
+
 int _isupper(int c)
+
 {
+
 if (c >= 65 && c <= 90)
 {
 return (1);
