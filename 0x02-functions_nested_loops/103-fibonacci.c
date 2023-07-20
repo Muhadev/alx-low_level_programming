dc846bfa5e0e9cf8f63a92cca6cebbe12a8d7ad6
Muhadev <muhammedfayemi@gmail.com>
2023-07-19 17:48:20 -0700

    103-fibonacci.c is added

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,7 +8,6 @@
  */
 int main(void)
 {
-int i = 0;
 long j = 1, k = 2, sum = k;
 while (k + j < 4000000)
 {
@@ -16,7 +15,6 @@ while (k + j < 4000000)
 if (k % 2 == 0)
 	sum += k;
 	j = k - j;
-	++i;
 }
 	printf("%ld\n", sum);
 return (0);
