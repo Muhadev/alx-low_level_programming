a0d887c1960f906fae4c73e5a3068c7989f5502b
Muhadev <muhammedfayemi@gmail.com>
2023-07-20 06:54:12 -0700

    0-main.c is added

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -9,17 +9,6 @@ int main(void)
 {
 int i;
 	i = 0;
-if (i > 0)
-{
-	printf("%d is positive\n", i);
-}
-else if (i == 0)
-{
-	printf("%d is zero\n", i);
-}
-else
-{
-	printf("%d is negative\n", i);
-}
+	positive_or_negative(i);
 return (0);
 }
