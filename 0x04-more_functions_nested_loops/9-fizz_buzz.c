53f9a528dc177025125aa8bc604d8290205729b4
Muhadev <muhammedfayemi@gmail.com>
2023-07-21 04:48:08 -0700

    9-fizz_buzz.c is added

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,16 +1,17 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
  * main - Prints a Fizz Buzz program
- *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
+
 int i;
 
-for (i = 1; i <= 100; i++) 
+for (i = 1; i <= 100; i++)
 {
 if ((i % 3 == 0) && (i % 5 == 0))
 {
@@ -32,10 +33,7 @@ if (i != 100)
 {
 	printf(" ");
 }
-
 }
-    
 printf("\n");
-
 return (0);
 }
