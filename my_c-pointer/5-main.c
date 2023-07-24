9179a31f6d64391fcc290912f3e822b768077945
Muhadev <muhammedfayemi@gmail.com>
2023-07-24 09:58:42 -0700

    5-main.c is added

---
+++ my_c-pointer/5-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - deferencing pointers
+ *
+ * return: Always 0.
+ */
+
+int main(void)
+{
+int n;
+int *p;
+
+n = 98;
+p = &n;
+	printf("value of 'n': %d\n", n);
+	printf("Address of 'n': %p\n", &n);
+	printf("value of 'p': %p\n", p);
+	*p = 402;
+	printf("value of 'n': %d\n ", n);
+return (0);
+}
