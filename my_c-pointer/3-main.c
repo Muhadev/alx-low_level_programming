eb635cb5d5ce881beb056c193044058de81e88f5
Muhadev <muhammedfayemi@gmail.com>
2023-07-24 09:29:51 -0700

    variable_pointer is added

---
+++ my_c-pointer/3-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -printing the address of ponter
+ *
+ * return: Always 0.
+ */
+int main(void)
+{
+int *p;
+	printf("Address of variable 'p': %p\n", &p);
+return (0);
+}
