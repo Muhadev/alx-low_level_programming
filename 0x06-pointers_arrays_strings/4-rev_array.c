f3ec3f74bdcdce6984b450e8483638829ec3acb2
Muhadev <muhammedfayemi@gmail.com>
2023-07-27 11:27:34 -0700

    4-rev_array.c is added

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: number of element of array
+ * return: void
+ */
+void reverse_array(int *a, int n)
+{
+int i;
+int j;
+for(1 = 0; i < n--; i++)
+{
+	t = a[i];
+	a[i] = a[n];
+	a[n] - t;
+}
+}
