c02c4a2415026873c3d5585d29b361debd44d240
Muhadev <muhammedfayemi@gmail.com>
2023-08-15 13:31:43 -0700

    2-main.c is added

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the name of 
+ * the file it was compiled from, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int main()
 {
 	printf("%s\n", __FILE__);
