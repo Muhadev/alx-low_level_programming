89336e6cf706a6ce78e8ba0ecbe11e89682c47a9
Muhadev <muhammedfayemi@gmail.com>
2023-08-31 02:54:21 -0700

    task is fix

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdlib.h>
 /**
  * struct listint_s - singly linked list
@@ -5,11 +7,10 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
