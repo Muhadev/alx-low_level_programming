f0ab751a5a6e078397a797b191f38a2f70577a22
Muhadev <muhammedfayemi@gmail.com>
2024-01-25 14:58:56 -0800

    5-main.py added

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -15,9 +15,7 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                perimeter += 4  # Each land cell contributes 4 to the perimeter
-
-                # Check neighboring cells and subtract 1 for each adjacent land cell
+                perimeter += 4
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 1
                 if i < len(grid) - 1 and grid[i + 1][j] == 1:
