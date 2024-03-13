7cefc1414e1c684a3ab00d62d43274082106951b
Muhadev <muhammedfayemi@gmail.com>
2024-03-13 15:54:32 -0700

    task is fix

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,15 +6,7 @@
 #include <stdio.h>
 #include <math.h>
 
-/* STRUCTURES */
-/**
- * struct listint_s - singly linked list
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
- */
+/* PROTOTYPES */
 int linear_search(int *array, size_t size, int value);
+
 #endif
